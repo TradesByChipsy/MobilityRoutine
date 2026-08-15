8024b7d2e35eea76a75dd0d733d9e827801cf491
Sven <s.unkelbach@gmx.de>
2026-08-15 20:41:17 +0200

    Add initial MobilityRoutine app files

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE='mobility-v1';
+const ASSETS=['./','./index.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  if(req.method!=='GET') return;
+  const url=new URL(req.url);
+  if(url.origin!==location.origin) return; // YouTube/Fonts: immer Netzwerk
+  e.respondWith(
+    caches.match(req).then(r=> r || fetch(req).then(res=>{
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return res;
+    }).catch(()=> caches.match('./index.html')))
+  );
+});
